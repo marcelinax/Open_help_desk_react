fcdbbec99d7c3b97a6e6a86523208774b3a2f49e
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-09-03 10:58:38 +0200

    OpenHelpDeskHelper ui  + create OpenHelpDeskHelper top bar

--- src/components/OpenHelpDeskHelper.js
+++ src/components/OpenHelpDeskHelper.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import OpenHelpDeskHelperProblems from './OpenHelpDeskHelperProblems';
 import OpenHelpDeskHelperComments from './OpenHelpDeskHelperComments';
 import { useSelector } from 'react-redux';
+import { useHistory } from 'react-router-dom';
 
 const OpenHelpDeskHelper = () => {
 
 	const selectedProblemId = useSelector(state => state.selectedProblemId.selectedProblemId);
+	const history = useHistory();
+	const [selectInput, setSelectInput] = useState('')
 
 	return (
 		<div className={'open-help-desk-helper'}>
-			<OpenHelpDeskHelperProblems/>
-			{selectedProblemId !== '' ? <OpenHelpDeskHelperComments/> : null}
+			<div className={'open-help-desk-helper-top'}>
+
+				<select>
+					<option value={'all-messages'}>All messages</option>
+					<option value={'new-messages'}>New messages</option>
+					<option value={'archived-messages'}>Archived messages</option>
+				</select>
+				<button onClick={() => {
+					history.push('/');
+				}}><i className="bx bx-exit"></i></button>
+			</div>
+			<div className={'open-help-desk-helper-main'}>
+				<OpenHelpDeskHelperProblems/>
+				{selectedProblemId !== '' ? <OpenHelpDeskHelperComments/> : null}
+			</div>
+
 		</div>
 	);
 };
--- src/components/OpenHelpDeskHelperComments.js
+++ src/components/OpenHelpDeskHelperComments.js
@@ -1,52 +1,61 @@
 import React, { useEffect, useState } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import OpenHelpDeskHelperCommentsListItem from './OpenHelpDeskHelperCommentsListItem';
+import { setSelectedProblemId } from '../state/selectedProblemSlice';
 
 const OpenHelpDeskHelperComments = () => {
-	const problems = useSelector(state => state.problems.problems);
-	const selectedProblemId = useSelector(state => state.selectedProblemId.selectedProblemId);
-	const [selectedProblem, setSelectedProblem] = useState({});
+		const problems = useSelector(state => state.problems.problems);
+		const selectedProblemId = useSelector(state => state.selectedProblemId.selectedProblemId);
+		const [selectedProblem, setSelectedProblem] = useState({});
+		const dispatch = useDispatch();
 
-	const getSelectedProblem = () => {
-		if (selectedProblemId !== '') {
-			setSelectedProblem(problems.filter(problem => problem.id === selectedProblemId)[0]);
-		}
-	};
+		const getSelectedProblem = () => {
+			if (selectedProblemId !== '') {
+				setSelectedProblem(problems.filter(problem => problem.id === selectedProblemId)[0]);
+			}
+		};
 
-	const renderMessages = () => {
-		return selectedProblem.problemDescriptions.map(problemDescription => (
-			<OpenHelpDeskHelperCommentsListItem avatarUrl={selectedProblem.avatarUrl} isProblem={problemDescription.type === 'problem'}
-																					comment={problemDescription.message}/>
-		));
-	};
+		const renderMessages = () => {
 
-	useEffect(() => {
-		getSelectedProblem();
-	}, [selectedProblemId]);
+			return Object.keys(selectedProblem).length > 0 ? selectedProblem.problemDescriptions.map(problemDescription => (
+				<OpenHelpDeskHelperCommentsListItem avatarUrl={selectedProblem.avatarUrl} isProblem={problemDescription.type === 'problem'}
+																						comment={problemDescription.message}/>
+			)) : null
+				;
+		};
 
-	return (
-		<div className={'open-help-desk-helper-comments'}>
-			<div className={'open-help-desk-helper-comments-top'}>
-				<div className={'open-help-desk-helper-comments-top-needy-person-box'}>
-					<h3>{selectedProblem.problemTitle}</h3>
-					<div className={'needy-person'}>
-						<p>From</p>
-						<p className={'nickname'}>{selectedProblem.nickname}</p>
+		useEffect(() => {
+			getSelectedProblem();
+		}, [selectedProblemId]);
+
+		return (
+			<div className={'open-help-desk-helper-comments'}>
+				<div className={'open-help-desk-helper-comments-top'}>
+					<div className={'open-help-desk-helper-comments-top-needy-person-box'}>
+						<h3>{selectedProblem.problemTitle}</h3>
+						<div className={'needy-person'}>
+							<p>From</p>
+							<p className={'nickname'}>{selectedProblem.nickname}</p>
+						</div>
+					</div>
+					<div className={'open-help-desk-helper-comments-top-needy-buttons-box'}>
+						<button><i className="bx bx-archive-in"></i></button>
+						<button onClick={() => {
+							dispatch(setSelectedProblemId({ problemId: '' }));
+
+						}}><i className="bx bx-x"></i></button>
 					</div>
 				</div>
-				<div className={'open-help-desk-helper-comments-top-needy-buttons-box'}>
-					<button><i className="bx bx-archive-in"></i></button>
+				<div className={'open-help-desk-helper-comments-main'}>
+					{renderMessages()}
+				</div>
+				<div className={'open-help-desk-helper-comments-write-comment'}>
+					<textarea placeholder={'Comment'}></textarea>
+					<button>Send</button>
 				</div>
 			</div>
-			<div className={'open-help-desk-helper-comments-main'}>
-				{renderMessages()}
-			</div>
-			<div className={'open-help-desk-helper-comments-write-comment'}>
-				<textarea placeholder={'Comment'}></textarea>
-				<button>Send</button>
-			</div>
-		</div>
-	);
-};
+		);
+	}
+;
 
 export default OpenHelpDeskHelperComments;
