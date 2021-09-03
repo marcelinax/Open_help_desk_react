97c6732d8511d86d01511506692d95d27b7a33ee
Marcelina Sitko <marcelina.sitko@interia.pl>
2021-09-03 21:42:03 +0200

    select style + show amount of problems

--- src/components/OpenHelpDeskHelper.js
+++ src/components/OpenHelpDeskHelper.js
@@ -8,6 +8,7 @@ import { setSelectedProblemFilter } from '../state/problemsFilterSlice';
 const OpenHelpDeskHelper = () => {
 
 	const selectedProblemId = useSelector(state => state.selectedProblemId.selectedProblemId);
+	const problems = useSelector(state => state.problems.problems);
 	const history = useHistory();
 	const dispatch = useDispatch();
 	const [problemFilter, setProblemFilter] = useState('');
@@ -20,19 +21,22 @@ const OpenHelpDeskHelper = () => {
 		dispatch(setSelectedProblemFilter({ problemFilter }));
 	};
 
+	
 	useEffect(() => {
-		getProblemFilter()
-	}, [problemFilter])
+		getProblemFilter();
+	}, [problemFilter]);
 
 	return (
 		<div className={'open-help-desk-helper'}>
 			<div className={'open-help-desk-helper-top'}>
 
 				<select onChange={handleProblemFilter} value={problemFilter}>
-					<option value={'all-messages'}>All messages</option>
-					<option value={'new-messages'}>New messages</option>
-					<option value={'archived-messages'}>Archived messages</option>
+					<option value={'all-messages'}>All messages ({problems.length})</option>
+					<option value={'new-messages'}>New messages ({problems.filter(problem => problem.isSolved === false).length})</option>
+					<option value={'archived-messages'}>Archived messages ({problems.filter(problem => problem.isSolved).length})</option>
 				</select>
+
+
 				<button onClick={() => {
 					history.push('/');
 				}}><i className="bx bx-exit"/></button>
